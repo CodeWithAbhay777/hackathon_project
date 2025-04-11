c2794d9401d3034c173f3c1abbebe181757aab03
akanksha2008-design <aakanksha.gupta.csd22@ggits.net>
2025-04-11 19:36:29 +0530

    test git

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,9 +1,7 @@
-import React from 'react'
+import React from "react";
 
 const Login = () => {
-  return (
-    <div>Login</div>
-  )
-}
+  return <div className="bg-red-400">Login</div>;
+};
 
-export default Login
+export default Login;
