ab6a65213015e3df4e21ff192e3830d00800fab0
abhi486448 <ag096129abhishek@gmail.com>
2025-04-11 12:42:59 +0530

    Added TailwindCSS

--- frontend/src/pages/LandingPage.jsx
+++ frontend/src/pages/LandingPage.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const LandingPage = () => {
   return (
-    <div>LandingPage</div>
+    <div className={`bg-[url(https://img.freepik.com/free-vector/tropical-leaves-background-zoom_23-2148580778.jpg?semt=ais_hybrid&w=740)] w-[100vw] h-[100vh] bg-center bg-cover` } >LandingPage</div>
   )
 }
 
