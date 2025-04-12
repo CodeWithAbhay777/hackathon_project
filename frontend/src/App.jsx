7063a5656b6458db94531e3ba4eecbc7d3e0ac4a
akanksha2008-design <aakanksha.gupta.csd22@ggits.net>
2025-04-12 17:35:22 +0530

    test git

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,30 +1,27 @@
-import { useState} from 'react'
-import {  Routes, Route, } from "react-router-dom";
-import './App.css'
-import LandingPage from './pages/LandingPage';
-import Login from './pages/Login';
-import Signup from './pages/Signup';
-import Portal from './pages/Portal';
-import Dashboard from './pages/Dashboard';
-import Profile from './pages/Profile';
-import List from './pages/List';
+import { Routes, Route } from "react-router-dom";
+import "./App.css";
+import LandingPage from "./pages/LandingPage";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import Portal from "./pages/Portal";
+import Dashboard from "./pages/Dashboard";
+import Profile from "./pages/Profile";
+import List from "./pages/List";
 
 function App() {
-  
-
   return (
     <>
       <Routes>
-        <Route path='/' element={<LandingPage />} />
-        <Route path='/login' element={<Login />} />
-        <Route path='/signup' element={<Signup />} />
-        <Route path='/portal' element={<Portal />} />
-        <Route path='/dashboard' element={<Dashboard />} />
-        <Route path='/profile' element={<Profile />} />
-        <Route path='/list' element={<List />} />
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/portal" element={<Portal />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/list" element={<List />} />
       </Routes>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
