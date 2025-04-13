1f0affdfe31ac9f25b6b4e8b5d3018bbc8575b90
abhi486448 <ag096129abhishek@gmail.com>
2025-04-14 01:25:28 +0530

    About and Nevbar3 done

--- frontend/src/components/LandingPageComponent/Hero.jsx
+++ frontend/src/components/LandingPageComponent/Hero.jsx
@@ -122,7 +122,7 @@ const Hero = () => {
 
           <video
             src={getVideoSrc(currentIndex == totalVideos - 1 ? 1 : currentIndex)}
-            autoPlay
+            // autoPlay
             loop
             muted
             className='absolute left-0 top-0 size-full object-cover object-center'
@@ -140,7 +140,7 @@ const Hero = () => {
 
             <p className='mb-5 max-w-64 font-robert-regular color-blue-100'>Enter the Metagame Layer <br /> Unlease the Play Economy</p>
 
-            <Button id='watch-trailer' title='Watch Trailer' leftIcon={<TiLocationArrow />} containerClass=' !bg-yellow-300 flex-center gap-1' />
+            <Button id='watch-trailer' title='Watch Trailer' leftIcon={<TiLocationArrow />} containerClass="!bg-yellow-300 flex-center gap-1" />
           </div>
         </div>
       </div>
